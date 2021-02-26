c6934732262a26eb51a729cf6ee0ef4345c2d207
kfalencik <kfalencik@gmail.com>
2021-02-26 18:28:54 +0000

    Messenger integration

--- plugins/vue-fb-customer-chat.js
+++ plugins/vue-fb-customer-chat.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueFbCustomerChat from 'vue-fb-customer-chat'
 
 Vue.use(VueFbCustomerChat, {
-  page_id: '259370984191819', //  change 'null' to your Facebook Page ID,
-  theme_color: '#d9a13a', // theme color in HEX
-  locale: 'en_US', // default 'en_US'
+  page_id: '109400771196516', //  change 'null' to your Facebook Page ID,
+  theme_color: '#ff4f33', // theme color in HEX
+  locale: 'en_GB', // default 'en_US'
 })
